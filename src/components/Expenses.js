55c28e674520c78ca5c04ea06dfcfdb9861e4878
ozturkdev <ahmetaress@gmail.com>
2023-04-02 21:04:34 +0300

    v3

---
+++ src/components/Expenses.js
@@ -0,0 +1,30 @@
+import ExpenselItem from "./Expenseltem";
+import Card from './Card'
+import './Expenses.css'
+
+function Expenses(props) {
+    return <Card className="expenses"> 
+        <ExpenselItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      ></ExpenselItem>
+      <ExpenselItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ExpenselItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+      <ExpenselItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </Card>
+}
+
+export default Expenses;
